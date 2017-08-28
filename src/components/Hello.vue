4c60a8746fd2e187a4a4d2e34ee5b9263a0fe4e5
Alex-Vasilev <aleksandr_vasilev1989@list.ru>
2017-08-28 18:12:17 +0300

    -

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="hello">
         <select name="" id="">
-            <option value=""></option>
+            <option value="" v-for='note in notes'>{{note.full_name}}</option>
         </select>
         <div v-for='note in notes'>
-            {{ note.text }}
+            {{note.full_name}}
         </div>
         <div>
             <button @click='addNew'>Добавить заметку</button>
@@ -13,17 +13,46 @@
 </template>
 
 <script>
+    import gql from 'graphql-tag';
+// GraphQL query
+    const postsQuery = gql`
+  query allRepos {
+    user(name: "facebook") {
+      name,
+      repositories(first: 5, orderby: FULL_NAME) {
+        name,
+        description
+      }
+    }
+  }
+`;
+
     export default {
         name: 'hello',
+        data: () => ({
+                posts: [],
+                loading: 0,
+                authorId: null,
+            }),
+        apollo: {
+            // Local state 'posts' data
+            posts: {
+                query: postsQuery,
+                loadingKey: 'loading',
+            },
+        },
         methods: {
             addNew() {
-                this.$store.dispatch('addNote', {text: 'новая заметка'});
+                this.$store.dispatch('addNote', "https://api.github.com/users/octocat/repos");
             }
 
         },
         computed: {
             notes() {
-                return this.$store.getters.notes;
+
+//                console.log(this.$store.getters.notes)
+//                return this.$store.getters.notes;
+                return this.posts
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -5,45 +5,67 @@ import App from './App'
 import router from './router'
 import Vuex from 'vuex'
 import VueResource from 'vue-resource'
+
+import ApolloClient, { createNetworkInterface, addTypename } from 'apollo-client';
+import VueApollo from 'vue-apollo';
+
 Vue.use(VueResource);
 
 Vue.use(Vuex); 
 
 Vue.config.productionTip = false;
 
+const apolloClient = new ApolloClient({
+  networkInterface: createNetworkInterface({
+//    uri: 'http://localhost:8080/graphql',
+    uri: 'https://api.github.com/users/octocat/repos',
+    transportBatching: true,
+  }),
+  queryTransformer: addTypename,
+  dataIdFromObject: r => r.id,
+});
+
+// Install the vue plugin
+// With the apollo client instance
+Vue.use(VueApollo, {
+  apolloClient,
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
 const store = new Vuex.Store({
     state: {
         notes: []
     },
     actions: {
-        addNote({commit}, note) {
+        addNote({commit}, url) {
 //            console.log(this)
-             return new Promise((resolve, reject) => {
+//             return new Promise((resolve, reject) => {
             // Do something here... lets say, a http call using vue-resource
-            Vue.http.get("https://api.github.com/users/octocat/repos").then(response => {
-                console.log(response)
+            Vue.http.get(url).then(response => {
+                console.log(response.body)
+                commit('ADD_NOTE', response.body)
                 // http success, call the mutator and change something in state
-                resolve(response);  // Let the calling function know that http is done. You may send some data back
+//                resolve(response);  // Let the calling function know that http is done. You may send some data back
             }, error => {
                 // http failed, let the calling function know that action did not work out
-                reject(error);
+                console.log(error);
             })
-        })
-//            this.$http.get('https://api.github.com/users/${name}/repos?per_page=${first}&sort=${orderby}').then(response => {
-//                    console.log(response);
-//                }, response => {
-//                    console.log(response);
-//                });
-//            commit('ADD_NOTE', note);
+//        })
         }
     },
     mutations: {
-        ADD_NOTE(state, note) {
-            state.notes.push(note);
+        ADD_NOTE(state, data) {
+            data.forEach(function(i){
+                state.notes.push(i);
+            })   
         }
     },
     getters: {
         notes(state) {
+            
             return state.notes;
         }
     }  
@@ -54,6 +76,7 @@ new Vue({
   el: '#app',
   router,
   store,
+  apolloProvider,
   template: '<App/>',
   components: { App }
 })
